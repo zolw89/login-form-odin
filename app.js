87f330164b544acc5eebcf1c4b9bb72772fc294b
Rafał Mosakowski <mosakowski.r@gmail.com>
2022-07-13 20:00:05 +0200

    login form

---
+++ app.js
@@ -0,0 +1,13 @@
+const password = document.getElementById("pass");
+const confirm_password = document.getElementById("confirm-pass");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
